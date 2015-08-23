7c07788a91c51543600064d4eec600b913bdbf20
Michelle Ann Mendoza <mendozamichelleann07@yahoo.com>
2015-08-23 23:47:46 +0800

    Code Igniter Setup Added to Repository

---
+++ assets/database/Simple_Reminder_Database.sql
@@ -0,0 +1,55 @@
+/*
+SQLyog Community v12.09 (64 bit)
+MySQL - 5.6.24 : Database - simple_reminder
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`simple_reminder` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `simple_reminder`;
+
+/*Table structure for table `reminder` */
+
+DROP TABLE IF EXISTS `reminder`;
+
+CREATE TABLE `reminder` (
+  `reminder_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) DEFAULT NULL,
+  `reminder_title` varchar(15) DEFAULT NULL,
+  `reminder_note` text,
+  `reminder_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `reminder_status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`reminder_id`),
+  KEY `username` (`username`),
+  CONSTRAINT `reminder_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `reminder` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(15) NOT NULL DEFAULT '',
+  `password` varchar(15) DEFAULT NULL,
+  `lname` varchar(15) DEFAULT NULL,
+  `fname` varchar(15) DEFAULT NULL,
+  `minit` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
