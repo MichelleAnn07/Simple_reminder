523fbb73a1a1d55f211652730735b73bf3fcca5a
Michelle Ann Mendoza <mendozamichelleann07@yahoo.com>
2015-08-27 13:59:36 +0800

    Model for the Reminder System added to Repo

---
+++ application/models/Reminder_model.php
@@ -0,0 +1,75 @@
+<?php
+class Reminder_model extends CI_Model{
+	public function __construct(){
+		$this->load->database();
+	}
+	//Checks if Username and Password exist in Database
+	public function check_login($username = null, $password = null){
+		$where = array('username' => $username, 'password' => $password);
+		$this->db->select();
+		$this->db->from('user');
+		$this->db->where($where);
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			return true;
+		}
+		else
+			return false;
+	}
+	//Gets the username
+	public function get_username($username = null){
+		$where = array('username' => $username);
+		$this->db->select('username');
+		$this->db->from('user');
+		$this->db->where($where);
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			return result_array;
+		}
+	}
+	//Gets Lastname and Firstname of user
+	public function get_name($username = null){
+		$where = array('username' => $username);
+		$this->db->select('fname,lname');
+		$this->db->from('user');
+		$this->db->where($where);
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			return $query->result_array();
+		}
+	}
+	//Gets the Reminder
+	public function get_reminder($username = null){
+		$where = array('username' => $username);
+		$this->db->select('reminder_title, reminder_note, reminder_timestamp, reminder_status');
+		$this->db->from('reminder');
+		$this->db->where($where);
+		$query = $this->db->get();
+		if($query->num_rows() > 0){
+			return $query->result_array();
+		}
+	}
+	//Insert New User
+	public function insert_user($username, $password, $lname, $fname, $email){
+		$user = array(
+			'username' => $username,
+			'password' => $password,
+			'lname' => $lname,
+			'fname' => $fname,
+			'email' => $email
+		);
+		$this->db->insert('user',$user);
+	}
+	//Insert New Reminder
+	public function insert_reminder($username, $reminder_title, $reminder_note, $reminder_timestamp, $reminder_status){
+		$reminder = array(
+			'username' => $username,
+			'reminder_title' => $reminder_title,
+			'reminder_note' => $reminder_note,
+			'reminder_timestamp' => $reminder_timestamp,
+			'reminder_status' => $reminder_status
+		);
+		$this->db->insert('reminder',$reminder);
+	}
+}
+?>
