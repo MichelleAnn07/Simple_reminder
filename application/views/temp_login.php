2bdb28ac82ae016aee9578a498aa6ae8382b7722
Raymar Monte <raymarmonte@gmail.com>
2015-08-27 23:38:13 +0800

    Added temp_login.php to views

---
+++ application/views/temp_login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Temporary Login Sign up</title>
+</head>
+<body>
+	<form method="post">
+		<h3>Login</h3>
+		Username:<input type="text" name="username" />
+		Password:<input type="password" name="password" />
+		<input type="submit" />
+	</form>
+	<form>
+		<h3>Sign-Up</h3>
+		Username:<input type="text" name="username" />
+		E-Mail:<input type="text" name="email" />
+		Password:<input type="password" name="password" />
+		<input type="submit" />
+	</form>
+</body>
+</html>
