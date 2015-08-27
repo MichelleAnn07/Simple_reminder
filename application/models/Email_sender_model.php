72abc1df76ef5bba76870d3c2042e6f8cc7696a2
Michelle Ann Mendoza <mendozamichelleann07@yahoo.com>
2015-08-27 13:55:08 +0800

    Model for Email Sender Added to Repo

---
+++ application/models/Email_sender_model.php
@@ -0,0 +1,25 @@
+<?php 
+class Email_sender_model extends CI_Model{
+	
+	public function __construct(){
+		$this->load->database();
+	}
+	
+	public function get_reminder($timestamp = null){
+		$where = array('reminder.reminder_timestamp <=' => $timestamp, 'reminder.reminder_status' => NULL);
+		$this->db->select('user.username, user.email, ,reminder.reminder_id, reminder.reminder_title, reminder.reminder_note');
+		$this->db->from('reminder');
+		$this->db->join('user', 'user.username = reminder.username');
+		$this->db->where($where);
+		$query = $this->db->get();
+		return $query->result();
+	}
+	
+	public function set_reminder_status($reminder_id = null){
+		$where = array('reminder_id' => $reminder_id);
+		$update = array('reminder_status' => 'SENT');
+		$this->db->where($where);
+		$this->db->update('reminder',$update);
+	}
+}
+?>
