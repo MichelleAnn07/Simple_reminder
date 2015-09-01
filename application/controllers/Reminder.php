ed0b5994bba6bcbe54a5f9f5a1377d486065ee33
Michelle Ann Mendoza <mendozamichelleann07@yahoo.com>
2015-09-01 21:16:18 +0800

    validate function added

--- application/controllers/Reminder.php
+++ application/controllers/Reminder.php
@@ -10,5 +10,33 @@ class Reminder extends CI_Controller{
 		$this->load->helper('url');
 		$this->load->view('landing');
 	}
+	
+	public function sign_in(){
+		$data('title') = 'Sign in';
+		
+	}
+	
+	public function validate_input($string, $type){
+		switch($type){
+			case 'name': 
+				if(empty($string))
+					return 'Field is required.';
+				if(!preg_match("/^[a-z ,.'-]+$/i", $string))
+					return 'Name is invalid.';
+				break;
+			case 'email':
+				if(empty($string))
+					return 'Field is required.';
+				if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+					return 'Email is invalid.';
+				break;
+		}
+	}
+	
+	public function filter_input($string){
+		$string = trim($data);
+		$string = htmlspecialchars($data);
+		return $data;
+	}
 }
 ?>
