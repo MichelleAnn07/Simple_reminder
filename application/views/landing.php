5f39d4e053503825a34154e8ea63b9d34a269a71
Raymar Monte <raymarmonte@gmail.com>
2015-09-03 13:12:12 +0800

    Added error messages to input fields of login page

--- application/views/landing.php
+++ application/views/landing.php
@@ -61,46 +61,40 @@
          <div class="sign-up">
             <div class="sign-up-center valign center-align">
                <h4 class="white-text">Sign-Up</h4>
-               <div class="login-fields">
-                  <?php
-                     echo $log_in_err;
-                  ?>
-               </div>
-               <br />
                <form method="post" action="<?php echo base_url(); ?>index.php/Reminder/sign_up">
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="username" type="text" class="validate white-text " name="username">
+                        <input <?php if(!empty($username_err)) echo 'placeholder="'.$username_err.'"'; ?> id="username" type="text" class="validate white-text <?php if(!empty($username_err)) echo 'invalid'; ?>" name="username">
                         <label for="username">Username</label>
                      </div>
                   </div>
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="password" type="password" class="validate white-text" name="password">
+                        <input <?php if(!empty($password_err)) echo 'placeholder="'.$password_err.'"'; ?> id="password" type="password" class="validate white-text <?php if(!empty($password_err)) echo 'invalid'; ?>" name="password">
                         <label for="password">Password</label>
                      </div>
                   </div>
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="repassword" type="password" class="validate white-text" name="repassword">
+                        <input <?php if(!empty($repassword_err)) echo 'placeholder="'.$repassword_err.'"'; ?> id="repassword" type="password" class="validate white-text <?php if(!empty($repassword_err)) echo 'invalid'; ?>" name="repassword">
                         <label for="repassword">Re-Type Password</label>
                      </div>
                   </div>
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="firstname" type="text" class="validate white-text " name="firstname">
+                        <input <?php if(!empty($firstname_err)) echo 'placeholder="'.$firstname_err.'"'; ?> id="firstname" type="text" class="validate white-text <?php if(!empty($firstname_err)) echo 'invalid'; ?>" name="firstname">
                         <label for="firstname">First Name</label>
                      </div>
                   </div>
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="lastname" type="text" class="validate white-text " name="lastname">
+                        <input <?php if(!empty($lastname_err)) echo 'placeholder="'.$lastname_err.'"'; ?> id="lastname" type="text" class="validate white-text <?php if(!empty($lastname_err)) echo 'invalid'; ?>" name="lastname">
                         <label for="lastname">Last Name</label>
                      </div>
                   </div>
                   <div class="row">
                      <div class="input-field signup-fields">
-                        <input id="email" type="email" class="validate white-text" name="email">
+                        <input <?php if(!empty($email_err)) echo 'placeholder="'.$email_err.'"'; ?> id="email" type="email" class="validate white-text <?php if(!empty($email_err)) echo 'invalid'; ?>" name="email">
                         <label for="email">E-Mail</label>
                      </div>
                   </div>
@@ -124,18 +118,20 @@
                <form method="POST" action="<?php echo base_url(); ?>index.php/Reminder/log_in/">
                  <div class="row">
                     <div class="input-field login-fields">
-                       <input id="username-log" type="text" class="validate white-text" name="username_log">
+                       <input <?php if(!empty($username_log_err)) echo 'placeholder="'.$username_log_err.'"'; ?> id="username-log" type="text" class="validate white-text <?php if(!empty($username_log_err)) echo 'invalid'; ?>" name="username_log">
                        <label for="username-log">Username</label>
                     </div>
                  </div>
                  <div class="row">
                     <div class="input-field login-fields">
-                       <input id="password-log" type="password" class="validate white-text" name="password_log">
+                       <input <?php if(!empty($password_log_err)) echo 'placeholder="'.$password_log_err.'"'; ?> id="password-log" type="password" class="validate white-text <?php if(!empty($password_log_err)) echo 'invalid'; ?>" name="password_log">
                        <label for="password-log">Password</label>
                     </div>
                  </div>
-                 <button class="btn waves-effect waves-light login-button" type="submit" name="action">Go!
-                 </button>
+                 <div class="row">
+                   <button class="btn waves-effect waves-light login-button" type="submit" name="action">Go!
+                   </button>
+                  </div>
                  <div class="section">
                     <p class="white-text btn-flat" id="moveleft">Not Registered Yet?<span class="sign-up-now"> Sign Up </span>Now</p>
                  </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 
 	$('input').focus(function() {
 		$(this).removeClass('valid');
+		$(this).removeAttr('placeholder');
 		$(this).removeClass('invalid');
 	});
 
