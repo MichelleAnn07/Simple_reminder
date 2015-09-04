307fd3f6d9fa70bdb3c87b1a098b4500b3fc579f
janrenz01 <janrenz01@hotmail.com>
2015-09-05 02:18:28 +0800

    Dashboard layout modified
    Added new css and javaScript for the new layout
    php code still up for editting

--- application/views/dashboard_proto1.php
+++ application/views/dashboard_proto1.php
@@ -1,136 +1,99 @@
 <!DOCTYPE html>
-
 <html>
-
-	<head>
-
-		<title>Simple Reminder | Home</title>
-	
-		<!--CSS-->
-		<link rel="stylesheet" type="text/css" href="css/vertical-Accordion.css">
-	</head>
-
-<style>
-body {
-	margin: 0;
-}
-
-#container {
-	margin: 20px auto;
-	width: 980px;
-}
-
-#accordion {
-	list-style:none;
-	margin:30px 0;
-	padding:0;
-	height:270px;
-	width:980px;
-	margin:0 0 0 11px;
-	border:2px solid #000000;
-	overflow:hidden;
-}
-.panel {
-	float:left;
-	display:block;
-	height:270px;
-	width:44px;
-	overflow:hidden;
-	color:#666666;
-	text-decoration:none;
-	font-size:16px;
-	line-height:1.5em
-}
-.panel.active {
-	width:848px
-}
-
-.panelContent {
-	padding:15px 15px 15px 55px;
-	height:240px;
-	width:778px;
-}
-.pink {
-	width:42px;
-	height:270px;
-	float:left;
-	background:url(../images/accordionSprite.png) no-repeat 4px 85px #f980a1;
-	border-right:2px solid #ffffff;
-	cursor:pointer
-}
-.dark1 {
-	background:url(../images/accordionSprite.png) no-repeat -58px 85px #e66486
-}
-.dark2 {
-	background:url(../images/accordionSprite.png) no-repeat -117px 86px #cf4266
-}
-.dark3 {
-	background:url(../images/accordionSprite.png) no-repeat -184px 87px #b71f45
-}
-.last {
-	border:none
-}
-.p1 {
-	background:url(../images/panel1.jpg) no-repeat left top
-}
-.p2 {
-	background:url(../images/panel1.jpg) no-repeat left top
-}
-.p3 {
-	background:url(../images/panel1.jpg) no-repeat left top
-}
-.p4 {
-	background:url(../images/panel1.jpg) no-repeat left top
-}
-</style>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.js"></script>
-<script>
-$(document).ready(function(){
- 
-    activePanel = $("#accordion div.panel:first");
-    $(activePanel).addClass('active');
- 
-    $("#accordion").delegate('.panel', 'click', function(e){
-         if( ! $(this).is('.active') ){
-			$(activePanel).animate({width: "44px"}, 300);
-			$(this).animate({width: "848px"}, 300);
-			$('#accordion .panel').removeClass('active');
-			$(this).addClass('active');
-			activePanel = this;
-		 };
-    });
-});
-</script>
+<head>
+	<title></title>
+	<link href="<?php echo base_url();?>assets/css/hTabMenu.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-<div id="container">
-<div id="accordion">
-    <div class="panel">
-      <div class="pink"></div>
-      <div class="panelContent p1"> <strong>Section 1 Header</strong><br/>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis volutpat quam, non suscipit arcu accumsan at. Aliquam pellentesque.
-      </div>
-    </div>
-    <div class="panel">
-      <div class="pink dark1"></div>
-      <div class="panelContent p2"> <strong>Section 2 Header</strong><br/>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis volutpat quam, non suscipit arcu accumsan at. Aliquam pellentesque.
-      </div>
-    </div>
-    <div class="panel">
-      <div class="pink dark2"></div>
-      <div class="panelContent p3"> <strong>Section 3 Header</strong><br/>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis volutpat quam, non suscipit arcu accumsan at. Aliquam pellentesque.
-      </div>
-    </div>
-    <div class="panel">
-      <div class="pink dark3"></div>
-      <div class="panelContent p4"> <strong>Section 4 Header</strong><br/>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis volutpat quam, non suscipit arcu accumsan at. Aliquam pellentesque.
-      </div>
-    </div>
+
+<nav class="nav nav--active">
+  <ul class="nav__list">
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color1" data-letter="a"></div>
+        <p class="nav__label">About</p>
+      </a>
+    </li>
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color2" data-letter="p"></div>
+        <p class="nav__label">Products</p>
+      </a>
+    </li>
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color3" data-letter="q"></div>
+        <p class="nav__label">Questions</p>
+      </a>
+    </li>
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color4" data-letter="e"></div>
+        <p class="nav__label">Events</p>
+      </a>
+    </li>
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color5" data-letter="s"></div>
+        <p class="nav__label">Sponsors</p>
+      </a>
+    </li>
+    <li class="nav__item">
+      <a href="" class="nav__link">
+        <div class="nav__thumb color6" data-letter="c"></div>
+        <p class="nav__label">Contact</p>
+      </a>
+    </li>
+  </ul>
+  <div class="burger burger--close">
+    <div class="burger__patty"></div>
+  </div>
+   <a href="http://ettrics.com/code/horizontal-tab-menu/" class="logo" target="_blank">
+   <img class="logo" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/45226/ettrics-logo.svg" alt="" /> 
+  </a>
+</nav>
+
+<div class="page">
+  <section class="section section--active color1" data-letter="a">
+    <article class="section__wrapper">
+      <h1 class="section__title">About us</h1>
+      <p>Use your 'left' and 'right' arrow keys to navigate.<br>Voluptatem necessitatibus aliquid cum repellat nihil praesentium tenetur vel voluptates cumque animi accusantium, deserunt, vitae est. Quas suscipit sint quidem fuga repudiandae.</p>
+    </article>
+  </section>
+  <section class="section color2" data-letter="p">
+    <article class="section__wrapper">
+      <h1 class="section__title">Products</h1>
+      <p>Use your 'left' and 'right' arrow keys to navigate.<br> Quos vel omnis quibusdam at inventore atque assumenda dignissimos ipsa magni perferendis, minima neque saepe reprehenderit quisquam numquam voluptas quo placeat quaerat!</p>
+    </article>
+  </section>
+  <section class="section color3" data-letter="q">
+    <article class="section__wrapper">
+      <h1 class="section__title">Questions</h1>
+      <p>Use your 'left' and 'right' arrow keys to navigate.<br> Labore iure tempora magnam aliquid voluptatum sit placeat necessitatibus, adipisci est, ipsum doloremque. Id quia consequatur labore repellendus. Ab eligendi voluptatibus doloribus.</p>
+    </article>
+  </section>
+  <section class="section color4" data-letter="e">
+    <article class="section__wrapper">
+      <h1 class="section__title">Events</h1>
+     <p>Use your 'left' and 'right' arrow keys to navigate.<br> Earum porro, at odit. Dolorem velit asperiores quam obcaecati ex numquam aspernatur at et! Possimus blanditiis, distinctio est qui deleniti nisi dolorem!</p>
+    </article>
+  </section>
+  <section class="section color5" data-letter="s">
+    <article class="section__wrapper">
+      <h1 class="section__title">Sponsors</h1>
+      <p>Use your 'left' and 'right' arrow keys to navigate.<br> Autem alias perferendis facilis, quibusdam, ratione repellendus, voluptate officiis ipsa ullam magnam libero atque doloribus sunt est ea nisi iste porro excepturi.</p>
+    </article>
+  </section>
+  <section class="section color6" data-letter="c">
+    <article class="section__wrapper">
+      <h1 class="section__title">Contact us</h1>
+      <p>Use your 'left' and 'right' arrow keys to navigate.<br> Dicta nostrum ad minima magnam, in nemo doloremque eum! Deserunt illo, laudantium eos ad, quisquam nisi, necessitatibus saepe ex a sit impedit.</p>
+    </article>
+  </section>
 </div>
-</div>
+
+
 </body>
-		<!--JavaScript-->
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/hTabMenu.js"></script>
 </html>
