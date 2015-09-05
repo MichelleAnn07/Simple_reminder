758a3632a14e4468bc2944405f09f8db4ad2f664
renzoralph07 <renzoralph07@hotmail.com>
2015-09-06 00:10:43 +0800

    hTabMenu Code Cleaning
    Removed useless lines of codes

--- assets/js/hTabMenu.js
+++ assets/js/hTabMenu.js
@@ -10,28 +10,6 @@ var Nav = (function() {
     isOpen  = true,
     hasT    = false;
   
-  var toggleNav = function() {
-    nav.toggleClass('nav--active');
-    burger.toggleClass('burger--close');
-    shiftPage();
-  };
-  
-  var shiftPage = function() {
-    if (!isOpen) {
-      page.css({
-        'transform': 'translateY(' + navH + 'px)',
-        '-webkit-transform': 'translateY(' + navH + 'px)'
-      });
-      isOpen = true;
-    } else {
-      page.css({
-        'transform': 'none',
-        '-webkit-transform': 'none'
-      });
-      isOpen = false;
-    }
-  };
-  
   var switchPage = function(e) {
     var self = $(this);
     var i = self.parents('.nav__item').index();
@@ -60,62 +38,8 @@ var Nav = (function() {
 
     return false;
   };
-  
-  var keyNav = function(e) {
-    var a = $('section.section--active');
-    var aNext = a.next();
-    var aPrev = a.prev();
-    var i = a.index();
-    
-    
-    if (!hasT) {
-      if (e.keyCode === 37) {
-      
-        if (aPrev.length === 0) {
-          aPrev = section.last();
-        }
-
-        hasT = true;
-
-        aPrev.addClass('section--active');
-        a
-          .addClass('section--hidden')
-          .removeClass('section--active');
-
-        a.on('transitionend webkitTransitionend', function() {
-          a.removeClass('section--hidden');
-          hasT = false;
-          a.off('transitionend webkitTransitionend');
-        });
-
-      } else if (e.keyCode === 39) {
-
-        if (aNext.length === 0) {
-          aNext = section.eq(0)
-        } 
-
-
-        aNext.addClass('section--active');
-        a
-          .addClass('section--hidden')
-          .removeClass('section--active');
-
-        hasT = true;
-
-        aNext.on('transitionend webkitTransitionend', function() {
-          a.removeClass('section--hidden');
-          hasT = false;
-          aNext.off('transitionend webkitTransitionend');
-        });
-
-      } else {
-        return
-      }
-    }  
-  };
     
   var bindActions = function() {
-    burger.on('click', toggleNav);
     link.on('click', switchPage);
     $(document).on('ready', function() {
        page.css({
@@ -123,7 +47,6 @@ var Nav = (function() {
          '-webkit-transform': 'translateY(' + navH + 'px)'
       });
     });
-    $('body').on('keydown', keyNav);
   };
   
   var init = function() {
