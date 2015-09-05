c5f35e1dca76542b0b5b20aa6bcfc7ed3d17c7ae
renzoralph07 <renzoralph07@hotmail.com>
2015-09-05 18:35:16 +0800

    Create Form Added Again
    Create form added again, along with its css and js files

--- application/views/dashboard_proto1.php
+++ application/views/dashboard_proto1.php
@@ -3,6 +3,8 @@
 <head>
   <title></title>
   <link href="<?php echo base_url();?>assets/css/hTabMenu.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php echo base_url();?>assets/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="<?php echo base_url();?>assets/css/lolliclock.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
 
@@ -26,8 +28,43 @@
 <div class="page">
   <section class="section section--active color1" data-letter="a">
     <article class="section__wrapper">
-      <h1 class="section__title">Add</h1>
-      <p>Add Form Here<br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s6">
+              <input id="reminder_title" type="text" class="validate">
+              <label for="reminder_title">Reminder Title</label>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="input-field col s12">
+              <textarea id="reminder_content" class="materialize-textarea"></textarea>
+              <label for="reminder_note">Reminder Content</label>
+            </div>
+          </div>
+
+            <div class="row">
+              <div class = "input-field col s12">
+                <input id="reminder_date" type="date" class="datepicker">
+              <label for="reminder_date">Reminder Date</label>
+              </div>
+            </div>
+
+            <div class="row">
+          <div class="input-field col s12">
+            <input id="reminder_time" type="text" class="lolliclock">
+            <label for="reminder_time">Reminder Time</label>
+          </div>
+        </div>
+
+            <div class="row">
+              <button class="btn waves-effect waves-light" type="submit" name="action">Set this Reminder
+              <!-- <i class="material-icons right">send</i> -->
+            </button>
+          </div>
+        </form>
+      </div>
     </article>
   </section>
   <section class="section color2" data-letter="v">
@@ -41,4 +78,7 @@
 </body>
   <script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url();?>assets/js/hTabMenu.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/materialize.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/lolliclock.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>assets/js/script.js"></script>
 </html>
