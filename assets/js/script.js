90e30decdb6ee29324210695008983c5dbd53dc2
renzoralph07 <renzoralph07@hotmail.com>
2015-09-05 17:59:10 +0800

    Revert "Create Form added"
    This reverts commit d4f3a57996596d99c5ad101a4876e28547fbc21e.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,3 @@ $( document ).ready(function() {
 	});
 
 });
-
-$('.datepicker').pickadate({
-
-	selectMonths: true, // Creates a dropdown to control month
-	selectYears: 15 // Creates a dropdown of 15 years to control year
-
-});
-
-$('#reminder_time').lolliclock({
-
-	autoclose:false
-
-});
